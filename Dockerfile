5ff2c507e9822c3b65d3da8941859db691cb03a8
Bhaumik Pandya <bhaumik.pandya21@gmail.com>
2019-03-08 15:36:28 +0100

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM node:10.5.0
 EXPOSE 8080
 COPY . .
+RUN wget https://github.com/apache-spark-on-k8s/spark/releases/download/v2.2.0-kubernetes-0.5.0/spark-2.2.0-k8s-0.5.0-bin-with-hadoop-2.7.3.tgz
+RUN tar -xzvf spark-2.2.0-k8s-0.5.0-bin-with-hadoop-2.7.3.tgz
+RUN ls
+RUN ls spark-2.2.0-k8s-0.5.0-bin-with-hadoop-2.7.3.tgz
+ENV SPARK_HOME spark-2.2.0-k8s-0.5.0-bin-with-hadoop-2.7.3.tgz
 CMD npm install
 CMD npm start
--- child_process/submit.sh
+++ child_process/submit.sh
@@ -1,4 +1,21 @@
-cd $spark_home
+export myscript=/home/bhaumik/test.py
+export driverimage=bhaumikpandya/spark-driver-py
+export executorimage=bhaumikpandya/spark-executor-py
+export initimage=bhaumikpandya/spark-init
+export tagname=latest
+export serviceAccountName=spark
+export appname=pysparkTest
+export driverPodName=sparkdriver
+export kubernetesNamespace=default
+export mongoUri=mongodb://apiomat:Qh0Zw47u5t2x1@158.177.122.67:28017/Spark.temp?authSource=admin
+export resourceStagingServerPort=10000
+export resourceStagingServerLoadBalancerName=spark-resource-staging-server-load-balancer
+export resourceStagingServerExternalIp=$(kubectl get svc -o jsonpath='{.items[?(@.metadata.name=="'${resourceStagingServerLoadBalancerName}'")].status.loadBalancer.ingress[*].ip}')
+export resourceStagingServerLoadBalancerPodName=$(kubectl get po -l resource-staging-server-instance=default -o jsonpath='{.items[*].metadata.name}')
+export resourceStagingServerInternalIp=$(kubectl get po -o jsonpath='{.items[?(@.metadata.name=="'$resourceStagingServerLoadBalancerPodName'")].spec.nodeName}')
+export resourceStagingServerInternalPort=31000
+export kubernetesuri=k8s://http://127.0.0.1:8001
+cd $SPARK_HOME
 bin/spark-submit \
   --packages=org.mongodb.spark:mongo-spark-connector_2.11:2.3.0 \
   --deploy-mode cluster \
