e05cfe0eac041512ff20763805559e45c5579a6b
Bhaumik Pandya <BPA@timetoact.group>
2019-03-08 13:52:29 +0100

    several changes

---
+++ scheduler/index.js
@@ -0,0 +1,149 @@
+const {
+  writeStatus,
+  updateStatus,
+  getStatus,
+  getActiveJobs,
+  getAllJobs,
+  getLastJobOfSubject
+} = require('./../job_logger');
+const {
+  aggregate2a,
+  aggregate2b,
+  aggregate2c
+} = require('../pipelines_aggregator');
+const {
+  startJob1b,
+  startJob1c,
+  getStatusForJob1b,
+  getStatusForJob1c
+} = require('../child_process');
+const schedule = require('node-schedule');
+
+let job = {
+  '1b': null,
+  '1c': null,
+  '2a': null,
+  '2b': null,
+  '2c': null
+};
+
+const SCHEDULE_2a = '* 5 6 * * *'; // '*/5 * * * *'; // '5 * * * * *'; // '* 5 6 * * *';
+const SCHEDULE_2b = '* 15 6 * * *';
+const SCHEDULE_2c = '* 30 6 * * *';
+const SCHEDULE_1b = '* 45 6 * * *';
+const SCHEDULE_1c = '* 0 7 * * *';
+
+function getStatusForJob(jobSubject, cb) {
+  if (job[jobSubject]) {
+    if(job[jobSubject].nextInvocation() == null){
+      return cb(null, {message: [{message: 'Job-' + jobSubject + ' was canceled.'}]});
+    } else {
+      return cb(null, {message: [{message: 'Job-' + jobSubject + ' is scheduled. Next Invocation at: '+ job[jobSubject].nextInvocation()}]});
+    }
+  } else {
+    return cb(null, {message: [{message: 'Job-' + jobSubject + ' is not scheduled.'}]});
+  }
+}
+
+// const jobb = schedule.scheduleJob('5 * * * * *', () => { console.log('hello\n\n') });
+// console.log(jobb);
+// console.log(jobb.triggeredJobs());
+// console.log(jobb.trackInvocation()); // throws error
+
+async function startJob(jobSubject, jobSchedule, jobFunction, cb) {
+  job[jobSubject] = schedule.scheduleJob(jobSchedule, jobFunction);
+
+  // rite current status in db
+  const jobStartedAt = Date.now();
+  await writeStatus({ 
+    jobStartedAt, 
+    status: 'Started', 
+    jobSubject, 
+    jobSchedule, 
+    nextExecution: job[jobSubject].nextInvocation(),
+    nTriggeredExecutions: job[jobSubject].triggeredJobs()
+  });
+
+  // update the status in db
+  const jobCompletedAt = Date.now();
+  await updateStatus({
+    jobStartedAt,
+    jobCompletedAt,
+    jobSubject,
+    status: 'Completed',
+    duration: jobCompletedAt - jobStartedAt,
+    active: false
+  });
+
+  return cb(null, {message: [{message: 'Job-' + jobSubject + ' started'}]});
+}
+
+function endJob(jobSubject, cb) {
+  if (!job[jobSubject]) {
+    return cb(null, {message: [{message: 'Job-' + jobSubject + ' was not scheduled so no cancellation required.'}]})
+  }
+  job[jobSubject].cancel();
+  if (job[jobSubject].nextInvocation() == null){
+    return cb(null, {message: [{message: 'Job-' + jobSubject + ' canceled'}]});
+  }
+  return cb(null, {message: [{message: 'Job-' + jobSubject + ' could not be canceled.'}]});
+}
+
+
+
+function startJob2a(cb) {
+  startJob('2a', SCHEDULE_2a, aggregate2a, cb);
+}
+
+function startJob2b(cb) {
+  startJob('2b', SCHEDULE_2b, aggregate2b, cb);
+}
+
+function startJob2c(cb) {
+  startJob('2c', SCHEDULE_2c, aggregate2c, cb);
+}
+
+
+
+function getStatusForJob2a(cb) {
+  getStatusForJob('2a', cb);
+}
+
+function getStatusForJob2b(cb) {
+  getStatusForJob('2b', cb);
+}
+
+function getStatusForJob2c(cb) {
+  getStatusForJob('2c', cb);
+}
+
+
+
+function endJob2a(cb) {
+  endJob('2a', cb);
+}
+
+function endJob2b(cb) {
+  endJob('2b', cb);
+}
+
+function endJob2c(cb) {
+  endJob('2c', cb);
+}
+
+
+module.exports = {
+  startJob1b,
+  startJob1c,
+  getStatusForJob1b,
+  getStatusForJob1c,
+  startJob2a,
+  startJob2b,
+  startJob2c,
+  getStatusForJob2a,
+  getStatusForJob2b,
+  getStatusForJob2c,
+  endJob2a,
+  endJob2b,
+  endJob2c
+};
